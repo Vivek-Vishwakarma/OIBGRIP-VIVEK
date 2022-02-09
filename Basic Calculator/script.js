d4084b22b4d2e28b17f511d811ffce0474c09dbb
Vivek-Vishwakarma <vivek.vishwakarma10022002@gmail.com>
2022-02-09 19:12:49 +0530

    added inputs

--- Basic Calculator/script.js
+++ Basic Calculator/script.js
@@ -1,15 +1,17 @@
 let screen = document.getElementById('screen')
 let buttons = document.querySelectorAll('button')
 let screenV = ''
+const viewer = document.querySelector('.viewer')
+
 for(item of buttons){
     item.addEventListener('click', (e) => {
         buttxt = e.target.innerText;
-        //console.log('Button text is ', buttxt);
         if(buttxt == 'AC'){
             screenV = '';
             screen.value = screenV;
         }
         else if (buttxt == '=') {
+            viewer.innerHTML = screenV
             screen.value = eval(screenV);
         }
         else if (buttxt == '^n') {
@@ -17,7 +19,7 @@ for(item of buttons){
             screenV += buttxt;
             screen.value = screenV;
         }
-        else if (buttxt == 'BackSpace'){
+        else if (buttxt == "BackSpace"){
             screen.value = screen.value.slice(0, - 1);
             screenV = screen.value;
         }
